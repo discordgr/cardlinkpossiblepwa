0c5c9e5b70380eeda6d14cd9d30993686627df52
discordgr <Razer_greece@yahoo.gr>
2020-04-21 17:01:51 +0300

    Quality of Life changes, Code clean up

--- src/main/java/gr/cardlink/pwa/MainView.java
+++ src/main/java/gr/cardlink/pwa/MainView.java
@@ -1,22 +1,13 @@
 package gr.cardlink.pwa;
 
 import com.vaadin.flow.component.Key;
-import com.vaadin.flow.component.Text;
-import com.vaadin.flow.component.UI;
-import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
-import com.vaadin.flow.component.checkbox.Checkbox;
-import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.dependency.JavaScript;
-import com.vaadin.flow.component.html.H3;
-import com.vaadin.flow.component.notification.Notification;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.textfield.NumberField;
-import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.PWA;
+import gr.cardlink.pwa.ui.PaymentView;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
@@ -32,9 +23,9 @@ import org.springframework.beans.factory.annotation.Autowired;
  * browser tab/window.
  */
 @Route
-@PWA(name = "Vaadin Application",
-        shortName = "Vaadin App",
-        description = "This is an example Vaadin application.",
+@PWA(name = "CardLink Payment Application",
+        shortName = "CardLink Payment App",
+        description = "This is a Payment application.",
         enableInstallPrompt = true)
 @CssImport("./styles/shared-styles.css")
 @CssImport(value = "./styles/vaadin-text-field-styles.css", themeFor = "vaadin-text-field")
@@ -46,84 +37,24 @@ public class MainView extends VerticalLayout {
      * <p>
      * Build the initial UI state for the user accessing the application.
      *
-     * @param service The message service. Automatically injected Spring managed bean.
+     * param service The message service. Automatically injected Spring managed bean.
      */
-    public MainView(@Autowired GreetService service) {
+    public MainView() {
 
-        H3 header = new H3("CardLink Possible");
-
-        //Currency List
-        ComboBox<String> currencies = new ComboBox<>("Currency");
-        currencies.setId("CurrencyList");
-        currencies.setItems("EUR");
-        currencies.setValue("EUR");
-
-        //Amount Layout
-        HorizontalLayout amountLayout = new HorizontalLayout();
-
-        Text totalAmount = new Text("€0,00");
-        NumberField amountNumberField = new NumberField("Amount");
-        amountNumberField.setId("Amount");
-
-        amountLayout.setAlignItems(Alignment.BASELINE);
-        amountLayout.add(amountNumberField, totalAmount);
-
-        amountNumberField.addValueChangeListener(k -> {
-            if ( amountNumberField.getValue() != null ) {
-                double amount = amountNumberField.getValue();
-                totalAmount.setText("€" + amount);
-            }
-        });
-
-        //Installments
-        NumberField installmentsNumberField = new NumberField("Installments");
-        installmentsNumberField.setValue(0.0);
-        installmentsNumberField.setId("Installments");
-
-        //Tip Layout
-        HorizontalLayout tipLayout = new HorizontalLayout();
-
-        Checkbox tipCheckBox = new Checkbox("Include Tip?");
-
-        Text tipAmount = new Text("€0,00");
-
-        NumberField tipNumberField = new NumberField("Tip");
-        tipNumberField.setId("Tip");
-
-        tipCheckBox.addValueChangeListener(t -> {
-            if ( t.getValue() ){
-                tipLayout.setVisible(true);
-            }else{
-                tipLayout.setVisible(false);
-            }
-        });
-
-        tipNumberField.addValueChangeListener(t -> {
-            tipAmount.setText("€" + tipNumberField.getValue());
-        });
-
-        tipLayout.setAlignItems(Alignment.BASELINE);
-        tipLayout.add(tipNumberField, tipAmount);
-        tipLayout.setVisible(false);
-
-        //Checkout Button
-        Button checkOut = new Button("CheckOut");
-        checkOut.addClickListener(b -> {
-            UI.getCurrent().getPage().executeJs("goToMobileApp()");
-        });
+        PaymentView paymentView = new PaymentView();
 
         // Theme variants give you predefined extra styles for components.
         // Example: Primary button is more prominent look.
-        checkOut.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        paymentView.getCheckOut().addThemeVariants(ButtonVariant.LUMO_PRIMARY);
 
         // You can specify keyboard shortcuts for buttons.
         // Example: Pressing enter in this view clicks the Button.
-        checkOut.addClickShortcut(Key.ENTER);
+        paymentView.getCheckOut().addClickShortcut(Key.ENTER);
 
         // Use custom CSS classes to apply styling. This is defined in shared-styles.css.
         addClassName("centered-content");
 
-        add(header, currencies, amountLayout, installmentsNumberField, tipCheckBox, tipLayout ,checkOut);
+        add(paymentView.getHeader(), paymentView.getCurrencies(), paymentView.getAmountLayout(), paymentView.getInstallmentsNumberField(),
+                paymentView.getTipCheckBox(), paymentView.getTipLayout() ,paymentView.getSmsCheckBox(), paymentView.getSmsLayout(), paymentView.getCheckOut());
     }
-
 }
---
+++ src/main/java/gr/cardlink/pwa/ui/PaymentView.java
@@ -0,0 +1,182 @@
+package gr.cardlink.pwa.ui;
+
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.checkbox.Checkbox;
+import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.NumberField;
+
+public class PaymentView {
+
+    //Add components here in case we need to duplicate UI with other values.
+    H3 header;
+    ComboBox<String> currencies;
+    NumberField installmentsNumberField;
+
+    HorizontalLayout amountLayout;
+    NumberField amountNumberField;
+
+    HorizontalLayout tipLayout;
+    Checkbox tipCheckBox;
+    NumberField tipNumberField;
+
+    Checkbox smsCheckBox;
+    HorizontalLayout smsLayout;
+
+    Button checkOut;
+
+
+    public PaymentView() {
+        header = new H3("CardLink Possible");
+
+        currencies = new ComboBox<>("Currency");
+        setCurrencies();
+
+        amountLayout = new HorizontalLayout();
+        createAmountLayout();
+
+        installmentsNumberField = new NumberField("Installments");
+        setInstallmentsNumberField();
+
+        tipLayout = new HorizontalLayout();
+        tipCheckBox = new Checkbox("Include Tip?");
+        createTipLayout();
+
+        smsLayout = new HorizontalLayout();
+        smsCheckBox = new Checkbox("Send Receipt to mobile phone?");
+        createSMSLayout();
+
+        checkOut = new Button("CheckOut");
+        setCheckOut();
+    }
+
+
+
+    private void createAmountLayout() {
+        Text totalAmount = new Text("€0,00");
+        amountNumberField = new NumberField("Amount");
+        amountNumberField.setId("Amount");
+        amountNumberField.setValue(0.0);
+        amountNumberField.addFocusListener(k -> {
+            amountNumberField.clear();
+        });
+
+        amountLayout.setAlignItems(FlexComponent.Alignment.BASELINE);
+        amountLayout.add(amountNumberField, totalAmount);
+
+        amountNumberField.addValueChangeListener(k -> {
+            if ( amountNumberField.getValue() != null ) {
+                totalAmount.setText("€" + amountNumberField.getValue());
+            }
+        });
+    }
+
+    public void createTipLayout() {
+        Text tipAmount = new Text("€0,00");
+
+        tipNumberField = new NumberField("Tip");
+        tipNumberField.setId("Tip");
+        tipNumberField.setValue(0.0);
+        tipNumberField.addFocusListener(t -> {
+            tipNumberField.clear();
+        });
+
+        tipCheckBox.addValueChangeListener(t -> {
+            if ( t.getValue() ){
+                tipLayout.setVisible(true);
+            }else{
+                tipLayout.setVisible(false);
+            }
+        });
+
+        tipNumberField.addValueChangeListener(t -> {
+            if ( tipNumberField.getValue() != null ){
+                tipAmount.setText("€" +  tipNumberField.getValue());
+            }
+        });
+
+        tipLayout.setAlignItems(FlexComponent.Alignment.BASELINE);
+        tipLayout.add(tipNumberField, tipAmount);
+        tipLayout.setVisible(false);
+    }
+
+    private void createSMSLayout() {
+        NumberField phoneNumberField = new NumberField("Mobile Phone");
+        phoneNumberField.setId("phoneNumber");
+
+        smsCheckBox.addValueChangeListener(s -> {
+            if ( s.getValue() ){
+                smsLayout.setVisible(true);
+            }else{
+                smsLayout.setVisible(false);
+            }
+        });
+
+        smsLayout.setAlignItems(FlexComponent.Alignment.BASELINE);
+        smsLayout.add(phoneNumberField);
+        smsLayout.setVisible(false);
+    }
+
+    private void setCurrencies(){
+        currencies.setId("CurrencyList");
+
+        currencies.setItems("EUR");
+        currencies.setItems("DOL");
+
+        currencies.setValue("EUR");
+    }
+
+    public void setInstallmentsNumberField() {
+        installmentsNumberField.setValue(0.0);
+        installmentsNumberField.setId("Installments");
+    }
+
+    private void setCheckOut() {
+        checkOut.addClickListener(b -> {
+            if (smsCheckBox.isEnabled()) {
+                UI.getCurrent().getPage().executeJs("sendSMS()");
+            }
+            UI.getCurrent().getPage().executeJs("goToMobileApp()");
+        });
+    }
+
+    public H3 getHeader() {
+        return header;
+    }
+
+    public ComboBox<String> getCurrencies() {
+        return currencies;
+    }
+
+    public NumberField getInstallmentsNumberField() {
+        return installmentsNumberField;
+    }
+
+    public HorizontalLayout getAmountLayout() {
+        return amountLayout;
+    }
+
+    public HorizontalLayout getTipLayout() {
+        return tipLayout;
+    }
+
+    public Checkbox getTipCheckBox() {
+        return tipCheckBox;
+    }
+
+    public Checkbox getSmsCheckBox() {
+        return smsCheckBox;
+    }
+
+    public Button getCheckOut() {
+        return checkOut;
+    }
+
+    public HorizontalLayout getSmsLayout() {
+        return smsLayout;
+    }
+}
